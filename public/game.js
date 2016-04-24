9c9fadb8e8732e8e18039f44a4a5ebb8ef301d5f
Christopher Von Hoene <cvonhoen@purdue.edu>
2016-04-24 01:12:28 +0000

    Testing keyboard input

--- public/game.js
+++ public/game.js
@@ -7,5 +7,16 @@ function start() {
 document.onkeydown = keyDown;
 
 function keyDown(event) {
-	document.getElementById("lblTime").innerHTML = "Hey, it worked!";
+	// document.getElementById("lblTime").innerHTML = "Hey, it worked!";
+	event = event || window.event;
+	
+	if (event.keyCode == 37) {	// Left
+		document.getElementById("lblTime").innerHTML = "Left";
+	} else if (event.keyCode == 38) {	// Up
+		document.getElementById("lblTime").innerHTML = "Up";
+	} else if (event.keyCode == 39) {	// Right
+		document.getElementById("lblTime").innerHTML = "Right";
+	} else if (event.keyCode == 40) {	// Down
+		document.getElementById("lblTime").innerHTML = "Down";
+	}
 }
